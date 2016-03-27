5d4e75130b0d6a1152d5893058e3b5e59e732d43
Steffen A. Mork <sm@morknet.de>
2016-03-27 18:46:53 +0000

    * Maven integriert.
    git-svn-id: svn+ssh://svn.morknet.de/usr/local/subversion/mdsc3/trunk@9 b27f19a9-626d-45bd-9596-ff2725c3252d

---
+++ make-release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VERSION=3.0.0
+
+for FILE in de.morknet.mdsc3.*/pom.xml
+do
+	echo $FILE
+	sed -e "0,/${VERSION}-SNAPSHOT/s//${VERSION}/" -i ${FILE}
+done
+
+for FILE in de.morknet.mdsc3.*/feature.xml de.morknet.mdsc3.*/META-INF/MANIFEST.MF
+do
+	echo $FILE
+	sed -e "s/${VERSION}.qualifier/${VERSION}/g" -i ${FILE}
+done
+
+sed -e "s/%RELEASE%/true/g" -e "s/%SNAPSHOT%/false/g" p2.template >de.morknet.mdsc3.feature/p2.inf
---
+++ make-snapshot.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+VERSION_OLD=3.0.0
+VERSION_NEW=3.0.1
+
+for FILE in de.morknet.mdsc3.*/pom.xml
+do
+	echo $FILE
+	sed -e "0,/${VERSION_OLD}/s//${VERSION_NEW}-SNAPSHOT/" -i ${FILE}
+done
+
+for FILE in de.morknet.mdsc3.*/feature.xml de.morknet.mdsc3.*/META-INF/MANIFEST.MF
+do
+	echo $FILE
+	sed -e "s/${VERSION_OLD}/${VERSION_NEW}.qualifier/g" -i ${FILE}
+done
+
+for FILE in de.morknet.mdsc3.updatesite/*.xml
+do
+	echo $FILE
+	sed -e "s/${VERSION_OLD}.qualifier/${VERSION_NEW}.qualifier/g" -i ${FILE}
+done
+
+sed -e "s/%RELEASE%/false/g" -e "s/%SNAPSHOT%/true/g" p2.template >de.morknet.mdsc3.feature/p2.inf
