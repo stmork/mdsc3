bdb799eaaef36c06914ad76ad0a60ff238514117
Steffen A. Mork <linux-dev@morknet.de>
2016-03-28 09:34:12 +0000

    * Beautifier ergänzt.  * Beispielprojekt für Generator ergänzt.  * Workflows für Generator ergänzt.
    git-svn-id: svn+ssh://svn.morknet.de/usr/local/subversion/mdsc3/trunk@13 b27f19a9-626d-45bd-9596-ff2725c3252d

---
+++ de.morknet.mdsc3/src/de/morknet/mdsc3/postprocessor/unix/ConfBeautifier.java
@@ -0,0 +1,71 @@
+/*
+**
+**	$Revision$
+**	$Date$
+**	$Author$
+**	$Id$
+**
+**	Copyright (C) 2010 Steffen A. Mork
+**
+**	This program and the accompanying materials are made available under the
+**	terms of the Eclipse Public License v1.0.
+**
+**	The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+**
+**
+*/
+
+package de.morknet.mdsc3.postprocessor.unix;
+
+import org.eclipse.xpand2.output.FileHandle;
+
+import de.morknet.mdsc3.postprocessor.BaseBeautifier;
+
+
+public class ConfBeautifier extends BaseBeautifier {
+
+	ConfBeautifier(FileHandle info)
+	{
+		super(info);
+	}
+
+	@Override
+	protected void process()
+	{
+		for (String line : m_Input)
+		{
+			if (line.length() > 0)
+			{
+				if (line.equals("##"))
+				{
+					m_Output.add("#");
+				}
+				else if(line.equals("#"))
+				{
+					m_Output.add("");
+				}
+				else
+				{
+					m_Output.add(line);
+				}
+			}
+		}
+	}
+
+	static boolean isConfiguration(FileHandle info)
+	{
+		String name = info.getAbsolutePath();
+		
+		return
+			name.endsWith(".conf") ||
+			name.endsWith(".config") ||
+			name.endsWith(".cnf") ||
+			name.endsWith(".cfg") ||
+			name.endsWith(".cf") ||
+			name.endsWith(".dot") ||
+			name.equals("iftab") ||
+			name.equals("interfaces") ||
+			name.equals("sudoers") ||
+			name.startsWith("common-");
+	}
+}
---
+++ de.morknet.mdsc3/src/de/morknet/mdsc3/postprocessor/unix/EmptyLineRemover.java
@@ -0,0 +1,50 @@
+/*
+**
+**	$Revision$
+**	$Date$
+**	$Author$
+**	$Id$
+**
+**	Copyright (C) 2010 Steffen A. Mork
+**
+**	This program and the accompanying materials are made available under the
+**	terms of the Eclipse Public License v1.0.
+**
+**	The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+**
+**
+*/
+
+package de.morknet.mdsc3.postprocessor.unix;
+
+import org.eclipse.xpand2.output.FileHandle;
+
+import de.morknet.mdsc3.postprocessor.BaseBeautifier;
+
+
+public class EmptyLineRemover extends BaseBeautifier
+{
+	EmptyLineRemover(FileHandle info)
+	{
+		super(info);
+	}
+
+	@Override
+	protected void process()
+	{
+		for (String line : m_Input)
+		{
+			if (line.length() > 0)
+			{
+				m_Output.add(line);
+			}
+		}
+	}
+
+	static boolean isLog(FileHandle info)
+	{
+		String name = info.getAbsolutePath();
+		
+		return name.endsWith(".log") || name.endsWith("hosts");
+	}
+}
