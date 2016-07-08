c7c2f02c5bea6721773596a3cfe4881383200804
Steffen A. Mork <sm@morknet.de>
2016-07-08 12:59:51 +0000

    * Skripte korrigiert.
    git-svn-id: svn+ssh://svn.morknet.de/usr/local/subversion/mdsc3/trunk@54 b27f19a9-626d-45bd-9596-ff2725c3252d

--- bin/sync-release.sh
+++ bin/sync-release.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+DST=/var/www/maven/mdsc3
+
 chgrp -R  www-data  de.morknet.mdsc3.updatesite/target/repository/
 rsync -av --delete de.morknet.mdsc3.updatesite/target/repository/\
-	root@scorpius.morknet.de:/var/www/maven/mdsc3/release/
+	root@scorpius.morknet.de:${DST}/release/
+ssh root@scorpius.morknet.de chown -R jboss.www-data ${DST}/
+ssh root@scorpius.morknet.de chmod -R g+rwX ${DST}/
--- bin/sync-snapshot.sh
+++ bin/sync-snapshot.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+DST=/var/www/maven/mdsc3
+
 chgrp -R  www-data  de.morknet.mdsc3.updatesite/target/repository/
 rsync -av --delete de.morknet.mdsc3.updatesite/target/repository/\
-	root@scorpius.morknet.de:/var/www/maven/mdsc3/snapshot/
+	root@scorpius.morknet.de:${DST}/snapshot/
+ssh root@scorpius.morknet.de chown -R jboss.www-data ${DST}/
+ssh root@scorpius.morknet.de chmod -R g+rwX ${DST}/
