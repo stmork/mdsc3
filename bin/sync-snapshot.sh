7d2007d68e14ec97f746376f07fea5e8298d6e41
Steffen A. Mork <sm@morknet.de>
2016-09-25 16:27:15 +0000

    * Moved update sites.
    git-svn-id: svn+ssh://svn.morknet.de/usr/local/subversion/mdsc3/trunk@67 b27f19a9-626d-45bd-9596-ff2725c3252d

--- bin/sync-snapshot.sh
+++ bin/sync-snapshot.sh
@@ -7,3 +7,6 @@ rsync -av --delete de.morknet.mdsc3.updatesite/target/repository/\
 	root@scorpius.morknet.de:${DST}/snapshot/
 ssh root@scorpius.morknet.de chown -R jboss.www-data ${DST}/
 ssh root@scorpius.morknet.de chmod -R g+rwX ${DST}/
+
+rsync -av --delete de.morknet.mdsc3.updatesite/target/repository/\
+	stmork,mdsc3@web.sourceforge.net:/home/project-web/mdsc3/htdocs/updates/snapshot/
