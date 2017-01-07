708039beccfe4ac73e19ae2854fe3963084ea12c
Steffen A. Mork <sm@morknet.de>
2017-01-07 12:12:46 +0000

    * Version Bump.
    git-svn-id: svn+ssh://svn.morknet.de/usr/local/subversion/mdsc3/trunk@84 b27f19a9-626d-45bd-9596-ff2725c3252d

--- make-release.sh
+++ make-release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=3.0.5
+VERSION=3.0.6
 
 for FILE in de.morknet.mdsc3*/pom.xml
 do
--- make-snapshot.sh
+++ make-snapshot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-VERSION_OLD=3.0.5
-VERSION_NEW=3.0.6
+VERSION_OLD=3.0.6
+VERSION_NEW=3.0.7
 
 for FILE in de.morknet.mdsc3*/pom.xml
 do
