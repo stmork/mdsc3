81fc3b34ce9d21acc137e9586467c5508398d785
Steffen A. Mork <linux-dev@morknet.de>
2025-11-14 11:18:44 +0100

    Fixed new chown usage.

--- bin/sync-snapshot.sh
+++ bin/sync-snapshot.sh
@@ -7,7 +7,7 @@ chmod -R o+rX de.morknet.mdsc3.updatesite/target/repository/
 chgrp -R  www-data  de.morknet.mdsc3.updatesite/target/repository/
 rsync -av --delete de.morknet.mdsc3.updatesite/target/repository/\
 	root@scorpius.morknet.de:${DST}/snapshot/
-ssh root@scorpius.morknet.de chown -R jboss.www-data ${DST}/
+ssh root@scorpius.morknet.de chown -R jboss.www:data ${DST}/
 ssh root@scorpius.morknet.de chmod -R g+rwX ${DST}/
 
 rsync -av --delete de.morknet.mdsc3.updatesite/target/repository/\
